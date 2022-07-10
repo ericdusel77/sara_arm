af91cf9776f022d80798d731c5d3582318de4914
ericdusel <dusel.eric@gmail.com>
2022-07-10 12:12:48 -0400

    updates for ocs2 compatibility. rviz dummy only. subscribes to button_pose

---
+++ src/ocs2_sara.cpp
@@ -0,0 +1,126 @@
+/******************************************************************************
+Copyright (c) 2017, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+ * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+#include <ocs2_sara.h>
+
+#include <ocs2_msgs/mpc_observation.h>
+#include <ocs2_ros_interfaces/common/RosMsgConversions.h>
+
+namespace ocs2 {
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+TargetTrajectoriesSara::TargetTrajectoriesSara(::ros::NodeHandle& nodeHandle, const std::string& topicPrefix,
+                                                                         GaolPoseToTargetTrajectories gaolPoseToTargetTrajectories)
+    : gaolPoseToTargetTrajectories_(std::move(gaolPoseToTargetTrajectories)) {
+  // observation subscriber
+  auto observationCallback = [this](const ocs2_msgs::mpc_observation::ConstPtr& msg) {
+    std::lock_guard<std::mutex> lock(latestObservationMutex_);
+    latestObservation_ = ros_msg_conversions::readObservationMsg(*msg);
+  };
+  observationSubscriber_ = nodeHandle.subscribe<ocs2_msgs::mpc_observation>(topicPrefix + "_mpc_observation", 1, observationCallback);
+
+  // Trajectories publisher
+  targetTrajectoriesPublisherPtr_.reset(new TargetTrajectoriesRosPublisher(nodeHandle, topicPrefix));
+
+  cloud_input_sub_ = nodeHandle.subscribe("button_pose", 1, &TargetTrajectoriesSara::processFeedback, this);
+  // // create an interactive marker for our server
+  // menuHandler_.insert("Send target pose", boost::bind(&TargetTrajectoriesSara::processFeedback, this, _1));
+
+  // // create an interactive marker for our server
+  // auto interactiveMarker = createInteractiveMarker();
+
+  // // add the interactive marker to our collection &
+  // // tell the server to call processFeedback() when feedback arrives for it
+  // server_.insert(interactiveMarker);  //, boost::bind(&TargetTrajectoriesSara::processFeedback, this, _1));
+  // menuHandler_.apply(server_, interactiveMarker.name);
+
+  // // 'commit' changes and send to all clients
+  // server_.applyChanges();
+}
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+void TargetTrajectoriesSara::processFeedback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+  // Desired state trajectory
+  const Eigen::Vector3d position(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
+  const Eigen::Quaterniond orientation(msg->pose.orientation.w, msg->pose.orientation.x, msg->pose.orientation.y,
+                                       msg->pose.orientation.z);
+
+  // get the latest observation
+  SystemObservation observation;
+  {
+    std::lock_guard<std::mutex> lock(latestObservationMutex_);
+    observation = latestObservation_;
+  }
+
+  // get TargetTrajectories
+  const auto targetTrajectories = gaolPoseToTargetTrajectories_(position, orientation, observation);
+
+  // publish TargetTrajectories
+  targetTrajectoriesPublisherPtr_->publishTargetTrajectories(targetTrajectories);
+}
+
+}  // namespace ocs2
+
+
+using namespace ocs2;
+
+/**
+ * Converts the pose of the interactive marker to TargetTrajectories.
+ */
+TargetTrajectories goalPoseToTargetTrajectories(const Eigen::Vector3d& position, const Eigen::Quaterniond& orientation,
+                                                const SystemObservation& observation) {
+  // time trajectory
+  const scalar_array_t timeTrajectory{observation.time};
+  // state trajectory: 3 + 4 for desired position vector and orientation quaternion
+  const vector_t target = (vector_t(7) << position, orientation.coeffs()).finished();
+  const vector_array_t stateTrajectory{target};
+  // input trajectory
+  const vector_array_t inputTrajectory{vector_t::Zero(observation.input.size())};
+
+  return {timeTrajectory, stateTrajectory, inputTrajectory};
+}
+
+int main(int argc, char* argv[]) {
+  const std::string robotName = "mobile_manipulator";
+  ::ros::init(argc, argv, robotName + "_target");
+  ::ros::NodeHandle nodeHandle;
+
+  TargetTrajectoriesSara targetPoseCommand(nodeHandle, robotName, &goalPoseToTargetTrajectories);
+  ::ros::spin();
+
+  // Successful exit
+  return 0;
+}
